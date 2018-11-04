3692ca4898968536700a1d1ce2e3f307fd8efdb8
Uzumaki-Boruto <bbe.haminte@gmail.com>
2018-11-04 09:56:25 +0700

    Format code

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 var app = angular.module('genLogin', []);
 app.controller('myController', function ($scope, $window) {
 	var LoginData = {
-		"nick1":{
+		"nick1": {
 			"username": "binner",
 			"password": "binner"
 		}
 	};
 	// $scope.result =false;
-	$scope.Login=function () {
+	$scope.Login = function () {
 		for (var i = 0; i < 4; i++) {
 			[i]
 		}
-		$scope.result =false;
-		if ($scope.txtuser==LoginData.nick1["username"]&&$scope.txtpass==LoginData.nick1["password"]) {
-			$scope.user=LoginData.nick1["username"];
+		$scope.result = false;
+		if ($scope.txtuser == LoginData.nick1["username"] && $scope.txtpass == LoginData.nick1["password"]) {
+			$scope.user = LoginData.nick1["username"];
 			alert("Login Successfully!");
-			$scope.result =true;
-		}else{
+			$scope.result = true;
+		} else {
 			alert('Login Fail!');
 		}
 	}
