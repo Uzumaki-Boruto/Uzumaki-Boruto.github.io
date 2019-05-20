0b6367e14d67b79dd2385daf85235088dbec2f1d
binner99 <binlove9225@gmail.com>
2019-05-21 00:47:36 +0700

    s

--- js/controller/checkCtrl.js
+++ js/controller/checkCtrl.js
@@ -1,45 +1,35 @@
-var app = angular.module('genSQLCode', ['ngCookies'])
-.controller('checkCtrl', ['$cookies', function($cookies) {
-  // Retrieving a cookie
-  var favoriteCookie = $cookies.get('myFavorite');
-  // Setting a cookie
-  $cookies.put('myFavorite', 'oatmeal');
-  console.log(favoriteCookie);
+var app = angular.module('genSQLCode', []);
+app.controller('checkCtrl', ['$scope', '$window', function ($scope, $window) {
+    $scope.objUser = [{
+        username: "binner",
+        pass: "binner"
+    },
+    {
+        username: "admin",
+        pass: "admin"
+    }];
+    // $scope.accInfo = null;
+    $scope.accInfo = $window.accInfo;
+    $scope.testLogin = function () {
+        if ($scope.objUser.find(x => x.username == $scope.accInfo.username && x.pass == $scope.accInfo.pass) == null) {
+            window.location = "./index.html";
+        }
+    }
 }]);
-// var app = angular.module('genSQLCode', ['ngCookies']);
-// app.controller('checkCtrl', ['$cookies', '$window', function ($scope, $window) {
-//     $scope.objUser = [{
-//         username: "binner",
-//         pass: "binner"
-//     },
-//     {
-//         username: "admin",
-//         pass: "admin"
-//     }];
-
-//     $scope.accInfo = $window.accInfo;
-//     $scope.testLogin = function () {
-//         // Retrieving a cookie
-//         var favoriteCookie = $cookies.get('myFavorite');
-//         // Setting a cookie
-//         $cookies.put('myFavorite', 'oatmeal');
-//         console.log(favoriteCookie);
-//     }
-// }]);
 //JS
-// function getParameterByName(name, url) {
-//     if (!url) url = window.location.href;
-//     name = name.replace(/[\[\]]/g, '\\$&');
-//     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-//         results = regex.exec(url);
-//     if (!results) return null;
-//     if (!results[2]) return '';
-//     return decodeURIComponent(results[2].replace(/\+/g, ' '));
-// }
-// var username = getParameterByName('txtUser');
-// var passW = getParameterByName('txtPass');
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+var username = getParameterByName('txtUser');
+var passW = getParameterByName('txtPass');
 
-// if (username == null || username == '' || passW == null || passW == '') {
-//     window.location = "./index.html";
-// }
-// var accInfo = { username: username.toLowerCase(), pass: passW };
+if (username == null || username == '' || passW == null || passW == '') {
+    window.location = "./index.html";
+}
+var accInfo = { username: username.toLowerCase(), pass: passW };
